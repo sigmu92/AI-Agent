88235bfb236f2f639ee04286670f97186e31dd64
sigmu92 <maxwellw92@gmail.com>
2025-07-13 12:54:38 -0500

    add prompt input from command line

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    model = "gemini-2.0-flash-001"
+    
+    inputs = sys.argv
+    if len(inputs) != 2:
+        print("invalid inputs, please enter a prompt")
+        sys.exit(1)
+    contents = inputs[1]
+
+    response = client.models.generate_content(model=model, contents=contents)
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print("Hello from aiagent!")
 
 
